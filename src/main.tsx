99790e796c4bc28743e7e98e9eeede83f084692d
JoshuaTreeTours <adventurehummer@gmail.com>
2026-01-10 17:55:03 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
-      <h1>All Outdoor Adventures</h1>
-      <p>It builds. It deploys. It lives.</p>
-    </div>
-  );
-}
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
