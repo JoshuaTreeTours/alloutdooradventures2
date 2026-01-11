0e7dff9e9a4e24c67203244602e939c6d95d44e6
JoshuaTreeTours <adventurehummer@gmail.com>
2026-01-10 16:13:37 -0800

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
+      <h1>All Outdoor Adventures</h1>
+      <p>It builds. It deploys. It lives.</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
