f51f988369422ac093907c2a674d70a4b6570a8e
JoshuaTreeTours <adventurehummer@gmail.com>
2026-01-10 17:53:43 -0800

    Create app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { Route, Switch } from "wouter";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route>404</Route>
+    </Switch>
+  );
+}
