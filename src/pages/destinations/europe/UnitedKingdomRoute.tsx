2f40a4c5a2b7d9a307e5795266e63920ffa30d65
JoshuaTreeTours <adventurehummer@gmail.com>
2026-01-23 20:14:22 -0800

    Remove England destination references

---
+++ src/pages/destinations/europe/UnitedKingdomRoute.tsx
@@ -0,0 +1,5 @@
+import EuropeCountryRoute from "./EuropeCountryRoute";
+
+export default function UnitedKingdomRoute() {
+  return <EuropeCountryRoute params={{ countrySlug: "united-kingdom" }} />;
+}
