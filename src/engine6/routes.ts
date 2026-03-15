58adc12fa7985f54fa172d6ae4fe4249ff8fae51
JoshuaTreeTours <adventurehummer@gmail.com>
2026-03-15 09:50:09 -0700

    Restrict Engine6 Hilo route to pilot canonical slug

--- src/engine6/routes.ts
+++ src/engine6/routes.ts
@@ -1,8 +1,7 @@
 import { engine6HiloVolcanoRecord } from "./viator/records";
 
 export const ENGINE6_HILO_TOUR_ROUTE = engine6HiloVolcanoRecord.canonicalPath;
-export const ENGINE6_HILO_TOUR_ROUTE_PATTERN =
-  "/destinations/hawaii/hilo/tours/:tourSlug";
+export const ENGINE6_HILO_TOUR_ROUTE_PATTERN = ENGINE6_HILO_TOUR_ROUTE;
 
 export const ENGINE6_HILO_PREVIEW_ROUTE_PATTERN = "/engine6/hilo/:tourSlug";
 export const ENGINE6_HILO_PREVIEW_ROUTE =
