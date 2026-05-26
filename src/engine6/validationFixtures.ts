72fcbbee8831e1ae6c1f9ad46ffe2d0966e64c8f
JoshuaTreeTours <adventurehummer@gmail.com>
2026-05-26 15:40:06 -0700

    Add Engine6 Yosemite Sequoias Glacier Point adventure

--- src/engine6/validationFixtures.ts
+++ src/engine6/validationFixtures.ts
@@ -26,6 +26,7 @@ import specimen26719Payload from "../../data/engine6/viator/26719P8.exact-produc
 import specimen354611p1Payload from "../../data/engine6/viator/354611P1.exact-product.json";
 import specimen5615689p4Payload from "../../data/engine6/viator/5615689P4.exact-product.json";
 import specimen36001Payload from "../../data/engine6/viator/36001P1.exact-product.json";
+import specimen415653p2Payload from "../../data/engine6/viator/415653P2.exact-product.json";
 import specimen36001p14Payload from "../../data/engine6/viator/36001P14.exact-product.json";
 import specimen152424p1Payload from "../../data/engine6/viator/152424P1.exact-product.json";
 import specimen23068p2Payload from "../../data/engine6/viator/23068P2.exact-product.json";
@@ -477,6 +478,12 @@ const RAW_ENGINE6_VALIDATION_FIXTURES: Array<
     publicUrl:
       "https://www.viator.com/tours/San-Francisco/Yosemite-In-A-Day-Tour-from-San-Francisco/d651-36001P1",
     rawPayload: specimen36001Payload as Record<string, unknown>,
+  },
+  {
+    productCode: "415653P2",
+    publicUrl:
+      "https://www.viator.com/tours/Yosemite-National-Park/Yosemite-Sequoias-Glacier-Point-and-More-5-Person-Epic-Adventure/d5265-415653P2",
+    rawPayload: specimen415653p2Payload as Record<string, unknown>,
   },
   {
     productCode: "36001P14",
