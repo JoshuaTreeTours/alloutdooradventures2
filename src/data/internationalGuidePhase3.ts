cd6477685b6f56a50de8177adcf3bbfe759a1bb9
JoshuaTreeTours <adventurehummer@gmail.com>
2026-09-10 16:22:19 -0700

    Extend Phase III Paragon governance to Latin America and Australia

--- src/data/internationalGuidePhase3.ts
+++ src/data/internationalGuidePhase3.ts
@@ -1,26 +1,31 @@
 import type { GuideContent, GuideListItem } from "./guideData";
+import { getCanonicalDestinationCitySlug } from "./destinationAliases";
 import {
   deepenInternationalPoiNarrative,
   enhanceInternationalGuidePhase2,
   INTERNATIONAL_PARAGON_PHASE2_GUIDE_KEYS,
 } from "./internationalGuidePhase2";
 import { ASIA_PACIFIC_PHASE3_PROFILES } from "./internationalGuidePhase3/asiaPacific";
+import { AUSTRALIA_PHASE3_PROFILES } from "./internationalGuidePhase3/australia";
 import { CANADA_PHASE3_PROFILES } from "./internationalGuidePhase3/canada";
 import { EUROPE_CENTRAL_PHASE3_PROFILES } from "./internationalGuidePhase3/europeCentral";
 import { EUROPE_ITALY_PHASE3_PROFILES } from "./internationalGuidePhase3/europeItaly";
 import { EUROPE_NORDIC_UK_PHASE3_PROFILES } from "./internationalGuidePhase3/europeNordicUk";
 import { EUROPE_SPAIN_MAJOR_PHASE3_PROFILES } from "./internationalGuidePhase3/europeSpainMajor";
 import { EUROPE_SPAIN_REGIONAL_PHASE3_PROFILES } from "./internationalGuidePhase3/europeSpainRegional";
+import { LATIN_AMERICA_PHASE3_PROFILES } from "./internationalGuidePhase3/latinAmerica";
 import type { InternationalGuidePhase3Profile } from "./internationalGuidePhase3/types";
 
 const profiles: Record<string, InternationalGuidePhase3Profile> = {
   ...ASIA_PACIFIC_PHASE3_PROFILES,
+  ...AUSTRALIA_PHASE3_PROFILES,
   ...CANADA_PHASE3_PROFILES,
   ...EUROPE_CENTRAL_PHASE3_PROFILES,
   ...EUROPE_ITALY_PHASE3_PROFILES,
   ...EUROPE_NORDIC_UK_PHASE3_PROFILES,
   ...EUROPE_SPAIN_MAJOR_PHASE3_PROFILES,
   ...EUROPE_SPAIN_REGIONAL_PHASE3_PROFILES,
+  ...LATIN_AMERICA_PHASE3_PROFILES,
 };
 
 const MIN_SEED_SENTENCES = 2;
@@ -92,13 +97,19 @@ const assertFinalPoiQuality = (key: string, pois: GuideListItem[]) => {
   }
 };
 
+export const getInternationalPhase3ProfileKey = (
+  countrySlug: string,
+  citySlug: string,
+) =>
+  `${countrySlug}/${getCanonicalDestinationCitySlug(countrySlug, citySlug)}`;
+
 export const enhanceInternationalGuidePhase3 = (
   countrySlug: string,
   citySlug: string,
   guide: GuideContent,
 ): GuideContent => {
   const phase2 = enhanceInternationalGuidePhase2(countrySlug, citySlug, guide);
-  const key = `${countrySlug}/${citySlug}`;
+  const key = getInternationalPhase3ProfileKey(countrySlug, citySlug);
   const profile = profiles[key];
   if (!profile) return phase2;
 
